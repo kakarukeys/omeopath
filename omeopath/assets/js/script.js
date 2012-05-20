31b298002dc28baed85610f2431985afbcd87abc
Wong Jiang Fung <kakarukeys@localhost.localdomain>
2012-05-20 11:27:57 +0800

    added feedback function

--- omeopath/assets/js/script.js
+++ omeopath/assets/js/script.js
@@ -1,8 +1,11 @@
-/* Author:
-
+/* Author: UserVoice
+    for feedback button
 */
 
-
-
-
+var uvOptions = {};
+(function() {
+    var uv = document.createElement('script'); uv.type = 'text/javascript'; uv.async = true;
+    uv.src = ('https:' == document.location.protocol ? 'https://' : 'http://') + 'widget.uservoice.com/OVQmB8TUoNg6sTR8ZJmQ.js';
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(uv, s);
+})();
 
